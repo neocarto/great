4c1d22a6cfe4f08d27a8e22dfa5c4b0ef91a1a4d
neocarto <nicolas.lambert@ums-riate.fr>
2017-10-04 12:09:22 +0200

    shp encoding

--- R/shptogeojson.R
+++ R/shptogeojson.R
@@ -2,20 +2,17 @@
 
 library("sf")
 
-setwd("/var/www/html/d3leaflet")
+setwd("/var/www/html/great")
 
 version <- c(2006,2010,2013)
 level <- c(0,1,2,3)
 
-getwd()
 
 for(i in version){
   for(j in level){
   x <- paste("database/shp/nuts",i,"/nuts",j,"-geo-",i,".shp",sep="")
   shp <- st_read(x, quiet = T)
   output <- paste("database/geojson/nuts",i,"-level",j,".geojson",sep="")
-  st_write(shp, output, driver ="geojson",factorsAsCharacter = TRUE, delete_dsn=T)
+  st_write(obj = shp, dsn = output, driver ="geojson", factorsAsCharacter = TRUE, delete_dsn=T)
   }
 }
-
-
